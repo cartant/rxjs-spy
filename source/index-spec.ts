cfbee4cad964aec702ef755a44c1d6853323d809
Nicholas Jamieson <nicholas@cartant.com>
2017-07-05 12:50:27 +1000

    chore(build): Remove TypeScript 2.4 workaround.
    And bump the RxJS dependency to a minimum of 5.4.2.

--- source/index-spec.ts
+++ source/index-spec.ts
@@ -4,18 +4,6 @@
  * found in the LICENSE file at https://github.com/cartant/rxjs-spy
  */
 
-// Workaround for https://github.com/Microsoft/TypeScript/issues/16593
-// See https://stackoverflow.com/a/44813884/6680611
-
-import { Operator } from "rxjs/Operator";
-import { Observable } from "rxjs/Observable";
-
-declare module "rxjs/Subject" {
-    interface Subject<T> {
-        lift<R>(operator: Operator<T, R>): Observable<R>;
-    }
-}
-
 import "./add/operator/tag-spec";
 import "./match-spec";
 import "./plugin/let-plugin-spec";
