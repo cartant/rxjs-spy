a39ae920118799b10b4e6c38f34e8fdcb99b7e8b
Nicholas Jamieson <nicholas@cartant.com>
2017-07-05 16:06:15 +1000

    doc(license): Fix license URLs.

--- scripts/bundle-common.js
+++ scripts/bundle-common.js
@@ -1,7 +1,7 @@
 /**
  * @license Copyright © 2017 Nicholas Jamieson. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/cartant/firebase-thermite
+ * found in the LICENSE file at https://github.com/cartant/rxjs-spy
  */
 
 "use strict";
