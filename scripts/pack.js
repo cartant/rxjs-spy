67b2fa4d3a43dfecc9b47b535d48f20021aac349
Nicholas Jamieson <nicholas@cartant.com>
2017-11-08 18:58:40 +1000

    chore(build): Add publishConfig warning.

--- scripts/pack.js
+++ scripts/pack.js
@@ -13,4 +13,7 @@ const content = Object.assign(
     JSON.parse(fs.readFileSync("./package.json")),
     JSON.parse(fs.readFileSync("./package-dist.json"))
 );
+if (content.publishConfig && (content.publishConfig.tag !== "latest")) {
+    console.warn(`\n\nWARNING: package.json contains publishConfig.tag = ${content.publishConfig.tag}\n\n`);
+}
 fs.writeFileSync("./dist/package.json", JSON.stringify(content, null, 2));
