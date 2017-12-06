c9ed161448cf5d7c2cfcdbb760a6c0616ea6e028
Nicholas Jamieson <nicholas@cartant.com>
2017-12-06 12:37:42 +1000

    refactor(guards): Use constants.

--- source/devtools/guards.ts
+++ source/devtools/guards.ts
@@ -3,15 +3,23 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/cartant/rxjs-spy
  */
+import {
+    MESSAGE_BATCH,
+    MESSAGE_BROADCAST,
+    MESSAGE_CONNECT,
+    MESSAGE_DISCONNECT,
+    MESSAGE_REQUEST,
+    MESSAGE_RESPONSE
+} from "./constants";
 
 import { Batch, Broadcast, Message, Post, Request, Response } from "./interfaces";
 
 export function isBatch(message: Message): message is Batch {
-    return message.messageType === "batch";
+    return message.messageType === MESSAGE_BATCH;
 }
 
 export function isBroadcast(message: Message): message is Broadcast {
-    return message.messageType === "broadcast";
+    return message.messageType === MESSAGE_BROADCAST;
 }
 
 export function isPost(message: Message): message is Post {
@@ -19,17 +27,17 @@ export function isPost(message: Message): message is Post {
 }
 
 export function isPostRequest(message: Message): message is Post & Request {
-    return isPost(message) && (message.messageType === "request");
+    return isPost(message) && (message.messageType === MESSAGE_REQUEST);
 }
 
 export function isPostResponse(message: Message): message is Post & Response {
-    return isPost(message) && (message.messageType === "response");
+    return isPost(message) && (message.messageType === MESSAGE_RESPONSE);
 }
 
 export function isRequest(message: Message): message is Request {
-    return message.messageType === "request";
+    return message.messageType === MESSAGE_REQUEST;
 }
 
 export function isResponse(message: Message): message is Response {
-    return message.messageType === "response";
+    return message.messageType === MESSAGE_RESPONSE;
 }
