5e3507a86748b8b9c1a5225d717211b451ffce81
Nicholas Jamieson <nicholas@cartant.com>
2018-08-22 14:30:00 +1000

    test(share): Add failing test.
    https://github.com/cartant/rxjs-spy/issues/37

---
+++ source/issues-spec.ts
@@ -0,0 +1,55 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/rxjs-spy
+ */
+/*tslint:disable:no-unused-expression*/
+
+import { expect } from "chai";
+import { concat, NEVER, of } from "rxjs";
+import { share } from "rxjs/operators";
+import { create } from "./spy-factory";
+import { Spy } from "./spy-interface";
+
+const options = {
+    defaultPlugins: true,
+    keptDuration: -1,
+    keptValues: 4,
+    warning: false
+};
+
+describe.only("issues", () => {
+
+    let spy: Spy;
+
+    beforeEach(() => {
+
+        spy = create(options);
+    });
+
+    describe("37", () => {
+
+        it("should resubscribe to a shared source", () => {
+
+            const source = concat(of("foo"), NEVER);
+            const shared = source.pipe(share());
+
+            const values: string[] = [];
+            let subscription = shared.subscribe(value => values.push(value));
+            subscription.unsubscribe();
+
+            expect(values).to.deep.equal(["foo"]);
+
+            subscription = shared.subscribe(value => values.push(value));
+            subscription.unsubscribe();
+
+            expect(values).to.deep.equal(["foo", "foo"]);
+        });
+    });
+
+    afterEach(() => {
+
+        if (spy) {
+            spy.teardown();
+        }
+    });
+});
