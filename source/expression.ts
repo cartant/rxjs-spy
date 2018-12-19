04156ce094d471a144916313849cd728296006ee
Nicholas Jamieson <nicholas@cartant.com>
2018-12-20 06:14:56 +1000

    refactor(expression): Remove concat.

--- source/expression.ts
+++ source/expression.ts
@@ -9,30 +9,14 @@ export function compile(expression: string): { func: Function, keys: string[] } 
 
     // https://stackoverflow.com/a/28244500/6680611
 
-    const compiled = new (Function.bind.apply(Function, [null].concat([
+    const compiled = new (Function.bind.apply(Function, [
+        null,
         ...keys,
         `return ${replaced};`
-    ] as any)))();
+    ]))();
 
     const func = (record: Record<string, any>) => compiled(
         ...keys.map(key => record[key])
     );
     return { func, keys };
 }
-/*
-spy.query({
-  dead: r => r.incomplete && r.nextAge > 60,
-  user: r => /user\.ts$/.test(r.fileName)
-})
-spy.query("dead && user")
-*/
-/*
-spy.query({
-  dead: r => r.incomplete && r.nextAge > 60,
-  quiet: r => r.incomplete && r.nextAge > 10
-  frequency: r => Math.max(r.nextCount / (r.nextAge - r.subscribeAge), 0)
-});
-*/
-/*
-Mention - in the README - that IDs can be passed wherever a match is accepted.
-*/
