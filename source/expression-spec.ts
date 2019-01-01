74e146ed1be483525aef06847be23b77a1a70cf8
Nicholas Jamieson <nicholas@cartant.com>
2019-01-01 16:36:17 +1000

    feat(expression): Add compileOrderBy.

--- source/expression-spec.ts
+++ source/expression-spec.ts
@@ -5,36 +5,63 @@
 /*tslint:disable:no-unused-expression*/
 
 import { expect } from "chai";
-import { compile } from "./expression";
+import { compile, compileOrderBy } from "./expression";
 
 describe("expression", () => {
 
-    it("should evaluate the expression using the specfied record", () => {
-        const { func } = compile("a + b + c");
-        expect(func({ a: "A", b: "B", c: "C" })).to.equal("ABC");
+    describe("compile", () => {
+
+        it("should evaluate the expression using the specfied record", () => {
+            const { evaluator } = compile("a + b + c");
+            expect(evaluator({ a: "A", b: "B", c: "C" })).to.equal("ABC");
+        });
+
+        it("should relax ===", () => {
+            const { evaluator } = compile("a === b");
+            expect(evaluator({ a: "1", b: 1 })).to.be.true;
+        });
+
+        it("should relax !==", () => {
+            const { evaluator } = compile("a !== b");
+            expect(evaluator({ a: "1", b: 1 })).to.be.false;
+        });
+
+        it("should support functions", () => {
+            const { evaluator } = compile(`file('a') || file("b")`);
+            expect(evaluator({
+                file: (name: string) => name === "a"
+            })).to.be.true;
+        });
+
+        it("should support regular expressions", () => {
+            const { evaluator } = compile(`file(/a/)`);
+            expect(evaluator({
+                file: (name: RegExp) => name.test("abc")
+            })).to.be.true;
+        });
     });
 
-    it("should relax ===", () => {
-        const { func } = compile("a === b");
-        expect(func({ a: "1", b: 1 })).to.be.true;
-    });
+    describe("compileOrderBy", () => {
 
-    it("should relax !==", () => {
-        const { func } = compile("a !== b");
-        expect(func({ a: "1", b: 1 })).to.be.false;
-    });
+        it("should default to ascending", () => {
+            const { comparer, evaluator } = compileOrderBy("name");
+            expect(evaluator({ name: "alice" })).to.equal("alice");
+            expect(evaluator({ name: "bob" })).to.equal("bob");
+            expect(comparer({ name: "alice" }, { name: "bob" })).to.equal(-1);
+        });
 
-    it("should support functions", () => {
-        const { func } = compile(`file('a') || file("b")`);
-        expect(func({
-            file: (name: string) => name === "a"
-        })).to.be.true;
-    });
+        it("should support ascending", () => {
+            const { comparer, evaluator } = compileOrderBy("name asc");
+            expect(evaluator({ name: "alice" })).to.equal("alice");
+            expect(evaluator({ name: "bob" })).to.equal("bob");
+            expect(comparer({ name: "alice" }, { name: "bob" })).to.equal(-1);
+        });
 
-    it("should support regular expressions", () => {
-        const { func } = compile(`file(/a/)`);
-        expect(func({
-            file: (name: RegExp) => name.test("abc")
-        })).to.be.true;
+        it("should support descending", () => {
+            const { comparer, evaluator } = compileOrderBy("name desc");
+            expect(evaluator({ name: "alice" })).to.equal("alice");
+            expect(evaluator({ name: "bob" })).to.equal("bob");
+            expect(comparer({ name: "alice" }, { name: "bob" })).to.equal(1);
+        });
     });
 });
--- source/expression.ts
+++ source/expression.ts
@@ -3,7 +3,10 @@
  * can be found in the LICENSE file at https://github.com/cartant/rxjs-spy
  */
 
-export function compile(expression: string): { func: Function, keys: string[] } {
+export function compile(expression: string): {
+    evaluator: (context: any) => any,
+    keys: string[]
+} {
     const keys = expression.split(/[^a-z]+/i).filter(Boolean);
     const replaced = expression.replace(/===/g, "==").replace(/!==/g, "!=");
 
@@ -15,8 +18,31 @@ export function compile(expression: string): { func: Function, keys: string[] } 
         `return ${replaced};`
     ]))();
 
-    const func = (record: Record<string, any>) => compiled(
+    const evaluator = (record: Record<string, any>) => compiled(
         ...keys.map(key => record[key])
     );
-    return { func, keys };
+    return { evaluator, keys };
+}
+
+export function compileOrderBy(expression: string): {
+    comparer: (left: any, right: any) => number,
+    evaluator: (context: any) => any
+} {
+    let ascending = true;
+    const match = expression.match(/[a-z]\s+(asc|desc)(\s*)$/i);
+    if (match) {
+        const [, direction, trailing] = match;
+        ascending = direction.toLowerCase() === "asc";
+        expression = expression.substring(0, expression.length - direction.length - trailing.length);
+    }
+    const { evaluator } = compile(expression);
+    let comparer = (left: any, right: any) => {
+        const l = evaluator(left);
+        const r = evaluator(right);
+        return (l < r) ? -1 : (l > r) ? 1 : 0;
+    };
+    return {
+        comparer: ascending ? comparer : (l, r) => comparer(l, r) * -1,
+        evaluator
+    };
 }
