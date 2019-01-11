c099714c9411cd5ec72bc83c53352e10dce097ad
Nicholas Jamieson <nicholas@cartant.com>
2019-01-12 09:03:07 +1000

    test(query): Add innerNextCount test.

--- source/plugins/snapshot-plugin-query-spec.ts
+++ source/plugins/snapshot-plugin-query-spec.ts
@@ -240,6 +240,19 @@ describe("SnapshotPlugin#query", () => {
         });
     });
 
+    describe("innerNextCount", () => {
+
+        it("should match inner observable next counts", () => {
+            harness.outer.next(0);
+            let result = query("innerNextCount > 0");
+            expect(result).to.match(foundRegExp(0));
+            harness.inner.next(0);
+            result = query("innerNextCount > 0");
+            expect(result).to.match(foundRegExp(1));
+            expect(result).to.match(idRegExp(harness.mapped));
+        });
+    });
+
     describe("innerIncompleteCount", () => {
 
         it("should match observables with incomplete inner observables", () => {
